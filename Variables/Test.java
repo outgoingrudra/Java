a2092188ca522b4c0f77d678c4c2559ee5601728
Rudra Verma <rudraverma781@gmail.com>
2025-06-11 10:58:43 +0530

    Variables and its type

---
+++ Variables/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 200;
+        int i = 1000;
+        long l = 100000L;
+        float f = 12.34f;
+        double d = 123.456;
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+    }
+}
